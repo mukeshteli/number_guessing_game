354a3a7cd6626ab3cf55e14fbfcc30815813ab46
mukeshteli <mokshh8301@gmail.com>
2026-01-17 18:06:22 +0530

    Added number guessing game logic and replay option

--- number_guessing.java
+++ number_guessing.java
@@ -6,7 +6,7 @@ public class number_guessing{
     public  void start_game(){
         int chance = 5;
         //int random = (int)(Math.random() * (max - min + 1)) + min;
-        int number_to_guess = (int) (Math.random() * (1005)) + 1;
+        int number_to_guess = (int) (Math.random() * (101-1)) + 1;
         System.out.println("Welcome to the Number Guessing Game!");
 
         
@@ -39,6 +39,20 @@ public class number_guessing{
          if(chance==0){
           System.out.println("sorry! you have used all your tries. the correct number was "+number_to_guess);
          }
+        System.out.println("do you want to play again? (yes/no)");
+        String play_again = sc.next();
+        if(play_again.equals("yes")){
+                start_game();
+            }
+        else if(play_again.equals("no")){
+                System.out.println("Thank you for playing!");
+                
+            }
+        else{
+                System.out.println("Invalid input");
+            
+
+        }
         
         
 
@@ -48,24 +62,7 @@ public class number_guessing{
         number_guessing ng = new number_guessing();
         System.out.println("Starting the Number Guessing Game...");
         ng.start_game();
-        while(true){
-            System.out.println("Game Over. Thanks for playing! do you want to play again? (yes/no)");
-            String play_again = ng.sc.nextLine();
-            if(play_again == "yes"){
-                ng.start_game();
-            }
-            else if(play_again == "no"){
-                System.out.println("Thank you for playing!");
-                break;
-            }
-            else{
-                System.out.println("Invalid input! please enter yes or no");
-            
-
-        }
         
-        
-    }
     
     
-}
+}}
